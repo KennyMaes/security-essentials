6b33fc03670344445e1cece9f5c9946e63117db4
KennyMaes <kennymaes@live.be>
2024-06-05 22:21:01 +0200

    Fetch challenge and solve in one go with user interaction

--- hash-challenge.py
+++ hash-challenge.py
@@ -1,21 +1,27 @@
 import json
+import time
 
 import requests
 import base64
 from hashlib import blake2b
+
+
 # Function to solve hash challenge
 def solve_hash_challenge(challenge_id, message):
     # Decode base64
     message = base64.b64decode(message)
 
+    attempt = 0  # Track number of attempts
+    dots = ""  # String to hold dots
+
     # Find prefix to make blake2 hash with 2 leading bytes equal to 0
     prefix = b''
 
+    print(f"\rSolving...")
     while True:
         test_message = prefix + message
 
         hash_bytes = blake2b(test_message, digest_size=32).digest()
-        print(f"Hash: {hash_bytes[:4]}")  # Print first 4 bytes of hash
         if hash_bytes[0] == 0 and hash_bytes[1] == 0:
             break  # Found a prefix!
         prefix += b'\x00'  # Increment prefix when no prefix found to start hash with 2 leading bytes as 0
@@ -32,22 +38,26 @@ def solve_hash_challenge(challenge_id, message):
     response = requests.delete(url, data=json_string, headers=headers)
 
     # Print response to see if delete was a success
-    print(response.json())
-    print(response.status_code)
+    return response
 
 
 # Main function
 def main():
-    # TODO: Do a request to get the data and solve in one flow?
     # Hash challenge data
-    hash_challenge = {
-        "challengeId": "4124509614",
-        "message": "OMCGutwRy4Pd56RDDI7VuaaqeUjF0I9zxu2qVVUb4dSqzxreQeH2I4OXrQAM3nnP+0xkXRwkEMd62ffBVVn+lg==",
-        "attemptsRemaining": "3"
-    }
+    input("Press any key to fetch new Hash challenge")
+    print("\nFetch new challenge...")
+    hash_challenge_response = requests.post('https://g5qrhxi4ni.execute-api.eu-west-1.amazonaws.com/Prod/hash').json()
+    print(hash_challenge_response)
+    input("\nPress any key to try to solve this challenge")
 
     # Solve hash challenge
-    solve_hash_challenge(hash_challenge['challengeId'], hash_challenge['message'], hash_challenge['attemptsRemaining'])
+    solve_response = solve_hash_challenge(hash_challenge_response['challengeId'], hash_challenge_response['message'])
+
+    if solve_response.status_code == 200:
+        print("\nSuccessfully solved challenge")
+        print(f"message: {solve_response.json()['message']}")
+    else:
+        print("Something went wrong")
 
 
 if __name__ == "__main__":
